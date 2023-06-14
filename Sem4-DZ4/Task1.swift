cb89fd4d4d7af1ceb45dc490254d0d9fa2a4a3b8
LEXASPB78 <ashkreba@yandex.ru>
2023-06-14 11:04:55 +0300

    Task1

---
+++ Sem4-DZ4/Task1.swift
@@ -0,0 +1,20 @@
+// Задание 1. Создать перечисление с видами пиццы (хотя бы 4-5 кейсов).
+
+struct Pizza {
+    var name: String
+    var cost: Double    
+}
+
+func getPizza() {
+    print(\(name), \(cost))
+}
+
+var latte = Pizza(name: "Latte", cost: 110)
+var margaret = Pizza(name: "Margaret", cost: 112)
+var pepperoni = Pizza(name: "Pepperoni", cost: 113)
+var carbonara = Pizza(name: "Carbonara", cost: 114)
+
+latte.getPizza()
+margaret.getPizza()
+lapepperonitte.getPizza()
+carbonara.getPizza()
