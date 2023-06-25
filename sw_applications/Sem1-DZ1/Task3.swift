ba56ea52737f58d73ca5ee23efe56514ee98329e
LEXASPB78 <ashkreba@yandex.ru>
2023-06-25 20:13:06 +0300

    Task3

---
+++ sw_applications/Sem1-DZ1/Task3.swift
@@ -0,0 +1,44 @@
+// 3. Реализовать экран с использованием сториборд (проект 2).
+// Это форма реализации.
+// Цвета на усмотрение, экран должен быть приближен к схеме (расположение элементов).
+// В поля «Логин» и «Пароль» можно вводить текст.
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Устанавливаем заголовок контроллера представления
+        title = "Login"
+
+        // Устанавливаем цвет фона
+        view.backgroundColor = UIColor.white
+
+        // Настройка текстового поля для входа в систему
+        loginTextField.placeholder = "Enter your username"
+        loginTextField.borderStyle = .roundedRect
+        loginTextField.autocapitalizationType = .none
+        loginTextField.autocorrectionType = .no
+        loginTextField.spellCheckingType = .no
+        loginTextField.returnKeyType = .next
+
+        // Настройка текстового поля пароля
+        passwordTextField.placeholder = "Enter your password"
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.borderStyle = .roundedRect
+        passwordTextField.returnKeyType = .done
+
+        // Добавляем кнопку входа в систему на панель навигации
+        let loginButton = UIBarButtonItem(title: "Login", style: .plain, target: self, action: #selector(LoginViewController.loginButtonTapped))
+        navigationItem.rightBarButtonItem = loginButton
+    }
+
+    @objc func loginButtonTapped() {
+        // Обработка события нажатия кнопки входа в систему
+    }
+}
