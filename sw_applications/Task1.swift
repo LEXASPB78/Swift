737123ac383461351a3d6ff9e01784a64ed210b6
LEXASPB78 <ashkreba@yandex.ru>
2023-06-25 19:49:16 +0300

    Task1

---
+++ sw_applications/Task1.swift
@@ -0,0 +1,43 @@
+// 1. Создать два проекта.
+// В первом удалить все упоминания сториборд (проект 1).
+// Во втором оставить (проект 2).
+
+// Проект 1: Удалить сториборд
+// Удаление сториборда из AppDelegate
+
+// Удаление строки в import
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Удалить связь с Main.storyboard
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
+// Проект 2: Оставить сториборд
+// Оставить сториборд в проекте
+
+// Оставить строку в import
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Связать с Main.storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let initialViewController = storyboard.instantiateInitialViewController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
