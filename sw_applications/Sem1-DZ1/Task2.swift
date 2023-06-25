323bd45e442db6a07b7e5db990411834e870fcdc
LEXASPB78 <ashkreba@yandex.ru>
2023-06-25 20:11:59 +0300

    Task2

---
+++ sw_applications/Sem1-DZ1/Task2.swift
@@ -0,0 +1,61 @@
+// 2. Реализовать экран без использования сториборд (проект 1).
+// Это форма реализации.
+// Цвета на усмотрение, экран должен быть приближен к схеме (расположение элементов).
+// В поля «Логин» и «Пароль» можно вводить текст.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let loginTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Логин"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Пароль"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Войти", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        view.addSubview(loginTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+        
+        NSLayoutConstraint.activate([
+            loginTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            loginTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            loginTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            loginTextField.heightAnchor.constraint(equalToConstant: 30),
+            
+            passwordTextField.topAnchor.constraint(equalTo: loginTextField.bottomAnchor, constant: 20),
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 30),
+            
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 50),
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            loginButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+}
